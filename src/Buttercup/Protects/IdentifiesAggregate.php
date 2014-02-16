2a38fde615434d64c7802d4b66eb50c6e8a3cead
Mathias Verraes <mathias@verraes.net>
2014-02-16 21:47:07 +0100

    Document IdentifiesAggregate

--- src/Buttercup/Protects/IdentifiesAggregate.php
+++ src/Buttercup/Protects/IdentifiesAggregate.php
@@ -9,17 +9,20 @@ namespace Buttercup\Protects;
 interface IdentifiesAggregate
 {
     /**
+     * Creates an identifier object from a string representation
      * @param $string
      * @return IdentifiesAggregate
      */
     public static function fromString($string);
 
     /**
+     * Returns a string that can be parsed by fromString()
      * @return string
      */
     public function __toString();
 
     /**
+     * Compares the object to another IdentifiesAggregate object. Returns true if both have the same type and value.
      * @param $other
      * @return boolean
      */
