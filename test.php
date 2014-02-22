de97a9d413f5e8eaefdd52a194c13a0163fe9b58
Mathias Verraes <mathias@verraes.net>
2014-02-22 14:35:23 +0000

    Renamed some files

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 // TestFrameworkInATweet https://gist.github.com/mathiasverraes/9046427
 function it($m,$p){echo ($p?'✔︎':'✘')." It $m\n"; if(!$p){$GLOBALS['f']=1;}}function done(){if(@$GLOBALS['f'])die(1);}
 
-require_once __DIR__ . '/tests/Buttercup/Protects/Tests/02-IdentifiesAggregate.php';
-require_once __DIR__ . '/tests/Buttercup/Protects/Tests/03-DomainEvent.php';
+require_once __DIR__ . '/tests/Buttercup/Protects/Tests/01-IdentifiesAggregate.php';
+require_once __DIR__ . '/tests/Buttercup/Protects/Tests/02-DomainEvent.php';
 
 done();
