f4fa1d6e7e51e350fbcd693bcd532939964ed9ac
Mathias Verraes <mathias@verraes.net>
2014-03-02 06:31:31 -0500

    Immutable array (to be moved elsewhere)

---
+++ src/Buttercup/Protects/ArrayIsImmutable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Buttercup\Protects;
+
+use BadMethodCallException;
+
+final class ArrayIsImmutable extends BadMethodCallException
+{
+
+} 
---
+++ src/Buttercup/Protects/ImmutableArray.php
@@ -0,0 +1,80 @@
+<?php
+namespace Buttercup\Protects;
+
+use ArrayAccess;
+use Countable;
+use Iterator;
+use SplFixedArray;
+
+abstract class ImmutableArray extends SplFixedArray implements Countable, Iterator, ArrayAccess
+{
+    public function __construct(array $items)
+    {
+        parent::__construct(count($items));
+        $i = 0;
+        foreach($items as $item) {
+            $this->guardType($item);
+            parent::offsetSet($i++, $item);
+        }
+    }
+
+    /**
+     * Throw when the type of item is not accepted.
+     *
+     * @throws \InvalidArgumentException
+     * @param $item
+     * @return void
+     */
+    abstract protected function guardType($item);
+
+    final public function count()
+    {
+        return parent::count();
+    }
+
+    final public function current()
+    {
+        return parent::current();
+    }
+
+    final public function key()
+    {
+        return parent::key();
+    }
+
+    final public function next()
+    {
+        parent::next();
+    }
+
+    final public function rewind()
+    {
+        parent::rewind();
+    }
+
+    final public function valid()
+    {
+        return parent::valid();
+    }
+
+    final public function offsetExists($offset)
+    {
+        return parent::offsetExists($offset);
+    }
+
+    final public function offsetGet($offset)
+    {
+        return parent::offsetGet($offset);
+    }
+
+    final public function offsetSet($offset, $value)
+    {
+        throw new ArrayIsImmutable();
+    }
+
+    final public function offsetUnset($offset)
+    {
+        throw new ArrayIsImmutable();
+    }
+
+}
