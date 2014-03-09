4c16094d1531b9bc62d71a091d59def3bb1ce439
Mathias Verraes <mathias@verraes.net>
2014-03-09 15:19:50 +0100

    AggregateHistory implementation

--- src/Buttercup/Protects/AggregateHistory.php
+++ src/Buttercup/Protects/AggregateHistory.php
@@ -3,7 +3,7 @@
 namespace Buttercup\Protects;
 
 /**
- * Represents the complete history of an Aggregate instance
+ * An ordered list of events that represents the complete history of a single Aggregate's instance.
  */
 interface AggregateHistory extends DomainEvents
 {
@@ -11,4 +11,30 @@ interface AggregateHistory extends DomainEvents
      * @return IdentifiesAggregate
      */
     public function getAggregateId();
+}
+
+/**
+ * @todo move
+ */
+final class AggregateHistoryIMPL extends DomainEventsIMPL implements AggregateHistory
+{
+    public function __construct(IdentifiesAggregate $aggregateId, array $events)
+    {
+        /** @var $event DomainEvent */
+        foreach($events as $event) {
+            if(!$event->getAggregateId()->equals($aggregateId)) {
+                throw new CorruptAggregateHistory;
+            }
+        }
+        parent::__construct($events);
+    }
+
+
+    /**
+     * @return IdentifiesAggregate
+     */
+    public function getAggregateId()
+    {
+
+    }
 }
