96cce4a775486d0b5c03e7dd9b3fbdb04162bfbe
Mathias Verraes <mathias@verraes.net>
2014-03-09 15:20:07 +0100

    Clean up

--- src/Buttercup/Protects/ButtercupProtectsException.php
+++ src/Buttercup/Protects/ButtercupProtectsException.php
@@ -5,4 +5,4 @@ namespace Buttercup\Protects;
 interface ButtercupProtectsException
 {
 
-} 
+}
---
+++ src/Buttercup/Protects/CorruptAggregateHistory.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Buttercup\Protects;
+
+use Exception;
+
+final class CorruptAggregateHistory extends Exception implements ButtercupProtectsException
+{
+}
